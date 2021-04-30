01775054f187855bf64d8495b5e8581c0726ecf4
adamnguyen1998 <83429705+adamnguyen1998@users.noreply.github.com>
2021-04-29 23:20:13 -0400

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+echo "Welcome to computer science  society."
+#Adam Nguyen anguyen117@student.gsu.edu
+echo "Date is `date`"
+echo */ | wc
+echo $PATH
+echo $USER
+echo $SHELL
+echo "Printing disk usage"
+echo "`df`"
+echo "Please, could you loan me \$25.00?"
+echo "if x=2, x*x = 4, x/2 = 1"
+echo "`ls *.sh | grep c`"
+echo "Good Bye"
+echo "The current Hour is `date +"%H"`"
